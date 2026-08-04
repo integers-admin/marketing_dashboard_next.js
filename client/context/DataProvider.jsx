7101e2ad255e996c79eb0d7d5d45b367b107b6c1
Integers Insights <integers1252@gmail.com>
2026-08-04 12:03:06 +0530

    Update DataProvider.jsx

--- client/context/DataProvider.jsx
+++ client/context/DataProvider.jsx
@@ -2,6 +2,8 @@
 import { useEffect, useState } from "react";
 import DataContext from "./DataContext";
 //
+
+
 const DataProvider = ({ children }) => {
     const baseurl1 = "https://marketing-dashboard-node-backend.onrender.com";
     const baseurl2 = "https://admin-panel.integermarket.com";

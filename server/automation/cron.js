f97458fc23ccaec9e0c44ee2c8660d2ac890dc94
Integers Insights <integers1252@gmail.com>
2026-08-08 09:48:44 +0530

    Update cron.js

--- server/automation/cron.js
+++ server/automation/cron.js
@@ -1,4 +1,4 @@
-
+// //
 // import cron from "node-cron";
 // import { fetchUmangGA4Data, fetchUmangGlobalEmailerData, fetchUmangGoogleAdsData } from "../services/umang.service.js";
 // import { pool } from "../config/db.js";

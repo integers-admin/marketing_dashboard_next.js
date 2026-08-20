240c0b910518a147e5edabf24e8d907ac4d5e022
Integers Insights <integers1252@gmail.com>
2026-08-20 11:35:53 +0530

    Update cron.js

--- server/automation/cron.js
+++ server/automation/cron.js
@@ -6,8 +6,7 @@ import { fetchAadarGA4Data, fetchAadarGoogleAdsData } from "../services/aadar.se
 
 const automation = () => {
     console.log("automation started...");
-    // cron.schedule("0 17 * * *", async () => {
-    cron.schedule("* * * * *", async () => {
+    cron.schedule("0 17 * * *", async () => {
         console.log("cron started");
         console.log(
             "Time:",
